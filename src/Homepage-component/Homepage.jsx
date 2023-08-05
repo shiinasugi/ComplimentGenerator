a7a5204e314f9a9fec2ef30b31686f15c98b13a6
Shiina Sugioka <s.sugioka12@gmail.com>
2023-08-05 10:40:44 -0700

    Added Bootstrap

--- src/Homepage-component/Homepage.jsx
+++ src/Homepage-component/Homepage.jsx
@@ -1,10 +1,16 @@
-import styles from './HomepageStyles.jsx';
+import styles from "./HomepageStyles.jsx";
 
 function Homepage() {
   return (
-    <div style={styles.divStyleTest}>
-      <span>Hello</span>
-    </div>
+    <>
+      <div style={styles.divStyleTest}>
+        <span>Hello</span>
+      </div>
+
+      <div class="card p-2 m-5">
+        <p>testing bootstrap</p>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import styles from './indexStyles.jsx';
+
 import Homepage from "./Homepage-component/Homepage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));

e08d2791c7014615caffc827c81751e6754a4c9c
hoanghohohaha <hoangng@cs.washington.edu>
2023-08-05 17:44:04 -0700

    feat: add router & navbar

--- src/index.js
+++ src/index.js
@@ -3,10 +3,32 @@ import ReactDOM from "react-dom/client";
 // import styles from './indexStyles.jsx';
 
 import Homepage from "./Homepage-component/Homepage";
+import DefaultLayout from "./layout/default";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./testComp/home";
+import AboutPage from "./testComp/about";
+import MissionPage from "./testComp/mission";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+  },
+    {
+    path: "/about",
+    element: <AboutPage/>,
+  },
+      {
+    path: "/mission",
+    element: <MissionPage/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Homepage />
+    <DefaultLayout>
+      <RouterProvider router={router}/>
+    </DefaultLayout>
   </React.StrictMode>
 );
